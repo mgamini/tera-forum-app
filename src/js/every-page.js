aec93393f9e5c220dd328c3251cbb10d21065764
Garrett Amini <garrett@enmasse.com>
2012-02-24 18:07:54 -0800

    ready for debug

---
+++ src/js/every-page.js
@@ -0,0 +1,1 @@
+;forge.logging.info("This is executed on page load");function removeAllStyles() {  for (i=0; i < document.styleSheets.length; i++)     { document.styleSheets[i].disabled = true; }};
